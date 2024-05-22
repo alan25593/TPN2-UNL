2842bb37b78475ba427f6d753b70803575db1dd2
Alan Lampert <60844186+alan25593@users.noreply.github.com>
2024-05-22 07:58:56 -0300

    Separar apellido

--- contacto.php
+++ contacto.php
@@ -1,17 +1,23 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $nombre = trim($_POST['nombre']);
+    $nombreCompleto = trim($_POST['nombre']);
     $direccion = trim($_POST['direccion']);
     $telefono = trim($_POST['telefono']);
     $correo = trim($_POST['correo']);
     $consulta = trim($_POST['consulta']);
     
-    $nombreArchivo = str_replace(' ', '-', strtolower($nombre)) . '.txt';
+    // Separar el nombre y el apellido
+    $partes = explode(' ', $nombreCompleto, 2);
+    $nombre = isset($partes[0]) ? $partes[0] : '';
+    $apellido = isset($partes[1]) ? $partes[1] : '';
+    
+    // Crear el nombre del archivo con Apellido-Nombre
+    $nombreArchivo = str_replace(' ', '-', strtolower($apellido . '-' . $nombre)) . '.txt';
     $consultaConFecha = date('Y-m-d H:i:s') . " - " . $direccion . " - " . $telefono . " - " . $correo . " - " . $consulta .  "\n";
     
     file_put_contents($nombreArchivo, $consultaConFecha, FILE_APPEND);
     
-    echo json_encode(['message' => $nombre. ' su consulta fue enviada con éxito, en breves nos pondremos en contacto!']);
+    echo json_encode(['message' => $nombre . ' su consulta fue enviada con éxito, en breves nos pondremos en contacto!']);
     exit;
 }
 ?>
